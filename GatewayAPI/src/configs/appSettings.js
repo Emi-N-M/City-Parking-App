59f3b70f9b40f794bc669abd5865652a2e553b86
Emi-N-M <emilio-0111@hotmail.com>
2022-05-19 03:13:30 +0100

    GatewayAPI created. Backend restructuration in to microservices + gateway

---
+++ GatewayAPI/src/configs/appSettings.js
@@ -0,0 +1,9 @@
+
+// URL microservices
+const UserUrl =   'http://localhost:5001/'
+const ParkingUrl     =   'http://localhost:5002/'
+// ----------
+export default {
+    UserUrl,
+    ParkingUrl
+};
---
+++ GatewayAPI/src/configs/urls.js
@@ -0,0 +1,23 @@
+import appSettings from './appSettings.js';
+
+const userBaseUrl =`${appSettings.UserUrl}`
+const parkingBaseUrl = `${appSettings.ParkingUrl}`
+
+
+export const userUrls = {
+    addNewUser: `${userBaseUrl}signup/`,
+    obtainToken:  `${userBaseUrl}login/`,
+    getOneUser: `${userBaseUrl}`,
+    getAllUsers: `${userBaseUrl}readAll`
+
+}
+
+
+export const parkingUrls = {
+    getAllParkings: `${parkingBaseUrl}`,
+    addNewParking: `${parkingBaseUrl}`,
+    getOneParking:`${parkingBaseUrl}`,
+    addCar: `${parkingBaseUrl}`,
+    removeCar: `${parkingBaseUrl}`
+
+}
